5bffcf33d92e6f019e27c912efb8a6d046505234
Mukim <AM068526@Cerner.net>
2020-01-03 12:34:18 +0530

    initial commit

---
+++ src/main/java/com/in28minutes/springboot/tutorial/SpringBootWebApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
